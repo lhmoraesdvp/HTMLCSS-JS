177e70039177df83013961e8be366fe5a48c916c
luis henrique de moraes <moraes.luishenrique17@gmail.com>
2021-11-29 18:10:48 -0300

    c
    c

--- README.md
+++
@@ -1,2 +0,0 @@
-# HTMLCSS&JS
- cursso
--- page1.js
+++
@@ -1,3 +0,0 @@
-alert("teste");
-var svg = d3.select("#he").append("svg").attr("height", 200).attr("width", 500).attr('stroke', 'white').attr('stroke-width', 1);
-var temp =svg.append('rect').attr('width', 100).attr('height', 100).attr('x', 10).attr('y', 10).attr('fill', 'grey');
